ccd8aa78b8573dad42ef7c9f05f4bb42a12f43da
Ramon <kokusilva206@gmail.com>
2024-06-20 16:03:42 -0300

    comecei a mudar os status

--- fuçôes/status.py
+++ fuçôes/status.py
@@ -1,45 +1,37 @@
 class Personagem:
     def __init__(self):
         self.atributos = {
-            'Agilidade': 15,
-            'Inteligência': 12,
-            'Sabedoria': 14,
-            'Destreza': 18,
-            'Constituição': 10,
-            'Carisma': 8,
-            'Percepção': 16
+            "sanidade": 0,
+            'força': 0,
+            'inteligencia': 0,
+            'destreza': 0,
+            'resistencia': 0,
+            'sabedoria': 0,
+            'Carisma': 0,
+            'reflexo': 0
         }
 
     def aumentar_atributo(self, atributo):
         if atributo in self.atributos:
             self.atributos[atributo] += 1
 
-    def diminuir_atributo(self, atributo):
-        if atributo in self.atributos and self.atributos[atributo] > 0:
-            self.atributos[atributo] -= 1
-
     def mostrar_atributos(self):
         for atributo, valor in self.atributos.items():
             print(f"{atributo}: {valor}")
-
-meu_personagem = Personagem()
-
-for atributo in meu_personagem.atributos:
-    meu_personagem.aumentar_atributo(atributo)
-
-while meu_personagem.atributos['Agilidade'] > 10:
-    meu_personagem.diminuir_atributo('Agilidade')
+# area dos jogadores
+jogador1 = Personagem()
 
 
-while meu_personagem.atributos['Inteligência'] > 10:
-    meu_personagem.diminuir_atributo('Inteligência')
+
+for atributo in jogador1.atributos:
+    jogador1.aumentar_atributo(atributo)
 
 
-atributo = 'Carisma'
+tributo = 'Carisma'
 valor_desejado = 12
 while True:
-    meu_personagem.aumentar_atributo(atributo)
-    if meu_personagem.atributos[atributo] >= valor_desejado:
+    jogador1.aumentar_atributo(atributo)
+    if jogador1.atributos[atributo] >= valor_desejado:
         break
 
-meu_personagem.mostrar_atributos()
+jogador1.mostrar_atributos()
