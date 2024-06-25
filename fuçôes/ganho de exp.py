5dc3efc27cea2aa7c4fe18b36432a89cff2f592a
Ramon <kokusilva206@gmail.com>
2024-06-25 20:13:07 -0300

    terminar de mudar em casa

--- fuçôes/ganho de exp.py
+++ fuçôes/ganho de exp.py
@@ -4,42 +4,37 @@ logging.basicConfig(level=logging.INFO)
 
 class Jogador:
     EXPERIENCIA_PARA_NIVEL = 100
-    VIDA_INICIAL = 100
 
     def __init__(self, nome):
         self.nome = nome
-        self.__vida = self.VIDA_INICIAL
         self.nivel = 1
         self.experiencia = 0
+        self.experiencia_para_proximo_nivel = self.EXPERIENCIA_PARA_NIVEL
 
     def ganhar_experiencia(self, pontos):
         self.experiencia += pontos
-        logging.info(f"{self.nome} ganhou {pontos} pontos de experiência.")
+        logging.info(f"{self.nome} ganhou {pontos} pontos de experiência. Total: {self.experiencia}")
 
-        if self.experiencia >= self.EXPERIENCIA_PARA_NIVEL:
+        while self.experiencia >= self.experiencia_para_proximo_nivel:
+            self.experiencia -= self.experiencia_para_proximo_nivel
             self.subir_de_nivel()
 
     def subir_de_nivel(self):
         self.nivel += 1
-        self.experiencia = 0
         logging.info(f"{self.nome} subiu para o nível {self.nivel}!")
+        self.experiencia_para_proximo_nivel = int(self.EXPERIENCIA_PARA_NIVEL * 2**(self.nivel - 1))
+        logging.info(f"Experiência para o próximo nível: {self.experiencia_para_proximo_nivel}")
 
     def status(self):
         print(f"Nome: {self.nome}")
         print(f"Nível: {self.nivel}")
-        print(f"Vida: {self.__vida}/{self.VIDA_INICIAL}")
         print(f"Experiência: {self.experiencia}/{self.EXPERIENCIA_PARA_NIVEL}")
 
 
 jogador1 = Jogador("zero")
 
-print("Status inicial do jogador:")
-jogador1.status()
-print()
-
-jogador1.ganhar_experiencia(100)
 jogador1.ganhar_experiencia(100)
 
-print("\nStatus após ganhar experiência:")
+print("\n")
 jogador1.status()
 print()
